8714094163ede6b2f58189005d067e4f2b2407c5
Nadia <pe78iom@eduvaud.ch>
2026-03-12 14:22:26 +0100

    Task001 completed

--- ma291-campus-snack-bar-starter/src/assets/js/fetchSalesPoints.js
+++
@@ -1,4 +0,0 @@
-// TODO task003: implémenter une fonction similaire à fetchSnacks
-export default async function fetchSalesPoints() {
-  return [];
-}
--- ma291-campus-snack-bar-starter/src/assets/js/fetchSnacks.js
+++
@@ -1,9 +0,0 @@
-export default async function fetchSnacks() {
-  const response = await fetch('../../data/snacks.json');
-
-  if (!response.ok) {
-    throw new Error(`Unable to load snacks: ${response.status}`);
-  }
-
-  return response.json();
-}
